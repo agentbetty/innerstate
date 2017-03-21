c8cedb803be9340197320c2dfb0daf416bd2f350
Elizabeth Miller <elizabeth@Squishys-MacBook-Pro.local>
2017-03-21 09:05:33 -0700

    working on home page mostly, but still the framework

--- js/application.js
+++ js/application.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
+  // identity rotate
+  var terms = ["design strategist", "front-end developer", "creative director"];
+
+  function rotateTerm() {
+    var ct = $(".roles").data("term") || 0;
+    $(".roles").data("term", ct == terms.length -1 ? 0 : ct + 1).text(terms[ct]).fadeIn()
+                .delay(2500).fadeOut(750, rotateTerm);
+  }
+  $(rotateTerm);
+
 });
