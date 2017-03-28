70ad3aee106c035c8359c3c6f60ea96661f8693d
Elizabeth Miller <elizabeth@Squishys-MacBook-Pro.local>
2017-03-27 20:39:24 -0700

    building the work section, two steps forward, one step back

--- js/application.js
+++ js/application.js
@@ -1,13 +1,30 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  // identity rotate
-  var terms = ["design strategist", "front-end developer", "creative director"];
+//   // identity rotate
+//   var terms = ["design strategist", "front-end developer", "creative director"];
 
-  function rotateTerm() {
-    var ct = $(".roles").data("term") || 0;
-    $(".roles").data("term", ct == terms.length -1 ? 0 : ct + 1).text(terms[ct]).fadeIn()
-                .delay(2500).fadeOut(750, rotateTerm);
-  }
-  $(rotateTerm);
+//   function rotateTerm() {
+//     var ct = $(".roles").data("term") || 0;
+//     $(".roles").data("term", ct == terms.length -1 ? 0 : ct + 1).text(terms[ct]).fadeIn(750)
+//                 .delay(2500).fadeOut(750, rotateTerm);
+//   }
+//   $(rotateTerm);
 
-});
+// });
+
+(function() {
+
+    var quotes = $(".roles");
+    var quoteIndex = -1;
+    
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(500)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+    
+    showNextQuote();
+    
+})();
